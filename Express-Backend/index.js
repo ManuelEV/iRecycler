1f060c3a8474981e0e7897eb1d332202414bcbe2
Pablo Barría <p.barria02@ufromail.cl>
2019-10-27 20:41:58 -0300

    Rutas y conexión bd

--- Express-Backend/index.js
+++ Express-Backend/index.js
@@ -1,12 +1,59 @@
 const express = require('express')
 const app = express()
 const port = 7070
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
+
+// Conexión a la BD
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, ()=> {
+  console.log('Database Connected')
+})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+
+//Schema
+
+const PuntoSchema = mongoose.Schema(
+  {
+  nombre: String,
+  lat: String,
+  lon: String,
+  descripcion: String,
+  capacidadActual: String,
+}
+);
+
+const PuntoModel = mongoose.model('Punto', PuntoSchema);
+
+// Rutas
+
+//CRUD Agregar y get
 
 app.get('/', (req,res) => {
-    res.send('A Okay!')
+    res.send('Start!!!!')
+})
+
+//Devolver todos los puntos
+app.get("/api/punto", async (request, response) => {
+  try {
+      var result = await PuntoModel.find().exec();
+      response.send(result);
+  } catch (error) {
+      response('Error');
+  }
+});
+
+//Agregar puntos
+app.post('/api/punto', function (req, res) {
+  console.log(req.body)
+  res.send(req.body)
 })
 
 
+// Puerto escucha
 
 app.listen(port, () => {
     console.log('Servidor Iniciado')
