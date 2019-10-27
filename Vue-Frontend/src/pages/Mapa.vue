e659687d3f0d831565d9c29f924b763bb288d1c0
Manuel <manuelespv@gmail.com>
2019-10-27 19:04:41 -0300

    popup

--- Vue-Frontend/src/pages/Mapa.vue
+++ Vue-Frontend/src/pages/Mapa.vue
@@ -3,7 +3,18 @@
   <div class="row map">
     <l-map :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker :lat-lng="marker"></l-marker>
+      <l-marker :lat-lng="marker">
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
     </l-map>
   </div>
 
@@ -16,7 +27,8 @@
     LTileLayer,
     LMarker,
     LPolyline,
-    LLayerGroup
+    LLayerGroup,
+    LPopup
   } from "vue2-leaflet";
 
 
@@ -27,15 +39,24 @@
       LTileLayer,
       LMarker,
       LPolyline,
-      LLayerGroup
+      LLayerGroup,
+      LPopup
     },
     data() {
       return {
         zoom:13,
-        center: L.latLng(47.413220, -1.219482),
+        center: L.latLng(-38.7396500, -72.5984200),
         url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        marker: L.latLng(47.413220, -1.219482),
+        marker: L.latLng(-38.7396500, -72.5984200)
+      }
+    },
+    methods: {
+      showLongText() {
+        this.showParagraph = !this.showParagraph;
+      },
+      innerClick() {
+        alert("Click!");
       }
     }
 
