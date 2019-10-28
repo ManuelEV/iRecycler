5860efcc2145a0e33a5c2377db83da4e7f9d5b8d
GabrielGuzman05 <g.guzman05@ufromail.cl>
2019-10-28 16:38:10 -0300

    Update MisPuntos.vue

--- Vue-Frontend/src/pages/MisPuntos.vue
+++ Vue-Frontend/src/pages/MisPuntos.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
     <div class="row" v-for="(punto, index) in misPuntos" :key="index">
-      <chart-card
-        :title="punto.nombre"
-        :sub-title="punto.descripcion"
-        :chart-data="punto.data"
-        chart-type="Pie"
-      >
-        <span slot="footer">
-          <i class="ti-timer"></i> Campaign set 2 days ago
-        </span>
-        <div slot="legend">
-          <i class="fa fa-circle text-info"></i> Open
-          <i class="fa fa-circle text-danger"></i> Bounce
-          <i class="fa fa-circle text-warning"></i> Unsubscribe
-        </div>
-      </chart-card>
+      <div class="col-md-6 col-12">
+        <chart-card
+          :title="punto.nombre"
+          :sub-title="punto.descripcion"
+          :chart-data="punto.data"
+          chart-type="Pie"
+        >
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Libre
+            <i class="fa fa-circle text-warning"></i> Ocupado
+          </div>
+        </chart-card>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import axios from "axios";
-
+import ChartCard from "@/components/Cards/ChartCard.vue";
 export default {
+  components: {
+    ChartCard
+  },
   data() {
     return {
       misPuntos: [
         {
           nombre: "nombre mal escrito",
-          capacidad: 50,
-          data: { labels: ["Ocupado", "No Ocupado"], series: [50, 50] }
+          descripcion: "La peor descripcion",
+          capacidad: 60,
+          data: { labels: ["40%", "60%"], series: [40, 60] },
+          options: {}
         }
       ]
     };
