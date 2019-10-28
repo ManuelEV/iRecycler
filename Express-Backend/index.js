88e7be96874ea0505240eef10e3ed836e56a024b
Pablo Barría <p.barria02@ufromail.cl>
2019-10-27 23:29:34 -0300

    Refactory + GetAll funcionando

--- Express-Backend/index.js
+++ Express-Backend/index.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 7070
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
 require('dotenv/config')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+const puntosRouter = require('./routes/puntos')
+app.use('/puntos', puntosRouter)
 
 // Conexión a la BD
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, ()=> {
@@ -12,44 +21,11 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(bodyParser.json());
-
-//Schema
-
-const PuntoSchema = mongoose.Schema(
-  {
-  nombre: String,
-  lat: String,
-  lon: String,
-  descripcion: String,
-  capacidadActual: String,
-}
-);
-
-const PuntoModel = mongoose.model('Punto', PuntoSchema);
 
 // Rutas
 
-//CRUD Agregar y get
-
 app.get('/', (req,res) => {
     res.send('Start!!!!')
-})
-
-//Devolver todos los puntos
-app.get("/api/punto", async (request, response) => {
-  try {
-      var result = await PuntoModel.find().exec();
-      response.send(result);
-  } catch (error) {
-      response('Error');
-  }
-});
-
-//Agregar puntos
-app.post('/api/punto', function (req, res) {
-  console.log(req.body)
-  res.send(req.body)
 })
 
 
