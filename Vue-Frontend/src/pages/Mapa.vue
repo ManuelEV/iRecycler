20e46f7b0d906fc906da56bf39653d197a8d3028
Manuel <manuelespv@gmail.com>
2019-10-28 16:16:21 -0300

    Conexion mapa principal con BD

--- Vue-Frontend/src/pages/Mapa.vue
+++ Vue-Frontend/src/pages/Mapa.vue
@@ -3,20 +3,13 @@
   <div class="row map">
     <l-map :zoom="zoom" :center="center" @click="addMarker">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker :lat-lng="marker">
-        <l-popup>
-          <div @click="showLongText">
-            I am a popup
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
-          </div>
-        </l-popup>
-      </l-marker>
       <l-marker v-for="addM, index in markers" :lat-lng="addM">
-
+        <l-popup>
+          <b>Nombre:</b><br>
+          {{nombres[index]}} <br>
+          <b>Descripción</b><br>
+          {{descripciones[index]}}
+        </l-popup>
       </l-marker>
       <l-control class="example-custom-control">
         <div class="text-center">
@@ -65,7 +58,9 @@
         attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         marker: L.latLng(-38.7396500, -72.5984200),
         showParagraph: false,
-        markers: []
+        markers: [],
+        nombres: [],
+        descripciones: []
       }
     },
     methods: {
@@ -76,10 +71,32 @@
         const url = this.ruta+"/puntos";
         console.log(url);
 
+        let me = this;
+
         axios.get(url)
           .then(function (r) {
             const response = r.data;
             console.log(response);
+
+            //me.markers = response;
+            //console.log(this.marker);
+            //var x;
+            for (const i in response){
+              console.log(response[i].nombre);
+
+              const lat = response[i].latitud;
+              const long = response[i].longitud;
+
+              me.markers.push(L.latLng(lat, long));
+
+              const nombre = response[i].nombre;
+              const descripcion = response[i].descripcion;
+
+              me.nombres.push(nombre);
+              me.descripciones.push(descripcion);
+
+            }
+
           });
       },
       showLongText() {
@@ -89,7 +106,7 @@
         alert("Click!");
       },
       addMarker(event){
-        this.markers = [];
+        //this.markers = [];
         this.markers.push(event.latlng);
         console.log(event.latlng.lat);
         console.log(event.latlng.lng);
